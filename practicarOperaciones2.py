03f42770d520dad54bc11d93acb627ae07eed9b2
Lucas <116810958+Lumo27@users.noreply.github.com>
2025-06-25 16:29:43 -0300

    Arreglo: Se arreglo el ejercicio pudiendo elegir la dificultad y la operacion dentro del bucle, antes solo hacia enfasis en la operacion y no tenia ambas funcionalidades.

--- practicarOperaciones2.py
+++ practicarOperaciones2.py
@@ -1,57 +1,62 @@
 ##Ejercicio 06
 import random
-operacion= 0
-num1=0
-num2=0
-sumaUsuario=0
-puntos=0
+operacion = ""
+num1 = 0
+num2 = 0
+sumaUsuario = 0
+puntos = 0
+dificultad = ""
+estado = ""  ## Variable de estado para controlar la práctica y ponerle fin o seguir
 
-print("Bienvenido a la practica de operaciones, elija una opcion: ")
-print("1- Suma , 2- Resta, 3- Multiplicacion 4- Division")
-print("En caso de querer finalizar, luego de terminar la practica,ingrese la palabra Finalizar")
-estado="" ##Variable de estado para controlar la practica y ponerle fin o seguir
-while(estado!="finalizar"):
-    operacion = int(input("Ingrese un número para escojer la operacion"))
+print("Bienvenido a la práctica de operaciones. Elija una opción: ")
+print("'+' para Suma , '-' para Resta, '*' para Multiplicación, '/' para División")
+operacion = input("Ingrese el símbolo de la operación que desea practicar: ")
 
-    if (operacion==1):
+print("Ahora elija la dificultad: ")
+print("1 - Fácil")
+print("2 - Medio")
+print("3 - Difícil")
+dificultad = int(input("Ingrese el número correspondiente al nivel de dificultad: "))
+
+print("En caso de querer finalizar, luego de terminar la práctica, ingrese la palabra Finalizar")
+
+while(estado != "finalizar"):
+
+    puntos = 0 
+
+    if (dificultad == 1):
         for x in range(10):
-            num1=random.randint(0,25)
-            num2=random.randint(0,25)
-            print("¿Cuánto es la suma de los siguientes numeros? ",num1," + ",num2)
-            sumaUsuario=int(input("Ingrese su resultado: "))
-            if(sumaUsuario == (num1+num2)):
-                puntos+=1
-    elif(operacion==2):
+            num1 = random.randint(0, 25)
+            num2 = random.randint(0, 25)
+            print("¿Cuánto es el resultado de la siguiente operación? ", num1, operacion, num2)
+            sumaUsuario = int(input("Ingrese su resultado: "))
+            if (sumaUsuario == (eval(str(num1) + operacion + str(num2)))):
+                puntos += 1
+
+    elif (dificultad == 2):
         for x in range(10):
-            num1=random.randint(26,75)
-            num2=random.randint(26,75)
-            print("¿Cuánto es la resta de los siguientes numeros? ",num1," - ",num2)
-            sumaUsuario=int(input("Ingrese su resultado: "))
-            if(sumaUsuario == (num1-num2)):
-                puntos+=1
-    elif(operacion==3):
+            num1 = random.randint(26, 75)
+            num2 = random.randint(26, 75)
+            print("¿Cuánto es el resultado de la siguiente operación? ", num1, operacion, num2)
+            sumaUsuario = int(input("Ingrese su resultado: "))
+            if (sumaUsuario == (eval(str(num1) + operacion + str(num2)))):
+                puntos += 1
+
+    elif (dificultad == 3):
         for x in range(10):
-            num1=random.randint(76,150)
-            num2=random.randint(76,150)
-            print("¿Cuánto es el producto de los siguientes numeros? ",num1," * ",num2)
-            sumaUsuario=int(input("Ingrese su resultado: "))
-            if(sumaUsuario == (num1*num2)):
-                puntos+=1
-    elif(operacion==4):
-        for x in range(10):
-            num1=random.randint(76,150)
-            num2=random.randint(76,150)
-            print("¿Cuánto es la division de los siguientes numeros? ",num1," / ",num2)
-            sumaUsuario=int(input("Ingrese su resultado: "))
-            if(sumaUsuario == (num1/num2)):
-                puntos+=1
-    print("Fin de la practica!")
-    print("El puntaje final fue: ",puntos,"/10"," acertadas, felicitaciones!")
-    estado=input("Para finalizar escriba: 'Finalizar'")
-    estado=estado.lower()
-    if (estado=="finalizar"):
+            num1 = random.randint(76, 150)
+            num2 = random.randint(76, 150)
+            print("¿Cuánto es el resultado de la siguiente operación? ", num1, operacion, num2)
+            sumaUsuario = int(input("Ingrese su resultado: "))
+            if (sumaUsuario == (eval(str(num1) + operacion + str(num2)))):
+                puntos += 1
+
+    print("Fin de la práctica!")
+    print("El puntaje final fue: ", puntos, "/10 acertadas. Felicitaciones!")
+
+    estado = input("Para finalizar escriba: 'Finalizar', o presione Enter para seguir practicando: ")
+    estado = estado.lower()
+    if (estado == "finalizar"):
         print("Gracias por practicar!")
     else:
         print("Sigamos!")
-    
-
